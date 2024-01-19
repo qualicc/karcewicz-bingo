7588fe77830a4e236c55fad308d9e31038e087fc
Jakub Moszyński <lolin515@wp.pl>
2024-01-19 09:57:28 +0100

    19.01.1

--- index.php
+++ index.php
@@ -7,8 +7,19 @@ $one = new newEnter();
 <!DOCTYPE html>
 <html lang="pl">
 <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JBJXDXXMG2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JBJXDXXMG2');
+</script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Witaj w fascynującej grze online 'Karcewicz Bingo'! Przeżyj nieprzewidywalne wykłady profesora Karcewicza, odznaczając nietypowe pytania i zdobywaj bingo z humorem!">
     <title>Karcewicz Bingo</title>
     <link rel='icon' type='png' href="images/bingo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
